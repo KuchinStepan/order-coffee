d4cded2c62e1d7d479c97bb4b3f2a2ff7d653963
Stepan_Kuchin <stepa.ku4in@gmail.com>
2024-04-28 18:31:53 +0500

    task 2

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-
 let fieldset = document.querySelector('.beverage');
+const addButton = document.querySelector('.add-button');
+
 fieldset.id = 'fieldset1';
 let fieldsetCount = 1;
 
+function renumberFieldSets() {
+    let i = 1;
+    for (const fieldset of document.querySelectorAll('fieldset')) {
+        fieldset.querySelector('h4').innerText = `Напиток №${i++}`;
+    }
+}
+
+function removeFieldset(fieldset) {
+    if (fieldsetCount > 1) {
+        fieldset.remove();
+        fieldsetCount--;
+        renumberFieldSets();
+    }
+    return false;
+}
+
+const removeButton = document.querySelector('.remove-button');
+removeButton.onclick = function () { removeFieldset(fieldset); };
+
 function addNewFieldset() {
+    const form = document.querySelector('form');
     const newFieldset = fieldset.cloneNode(true);
     newFieldset.querySelector('h4').innerText = `Напиток №${++fieldsetCount}`;
     newFieldset.id = `fieldset${fieldsetCount}`;
     for (const checkbox of newFieldset.querySelectorAll('.checkbox-field > input[type="radio"]')) {
         checkbox.name = `milk${fieldsetCount}`;
     }
-    fieldset.insertAdjacentElement('afterend', newFieldset);
-    fieldset = newFieldset;
+    newFieldset.querySelector('.remove-button').onclick = function () { removeFieldset(newFieldset); };
+
+    form.insertBefore(newFieldset, addButton.parentNode);
 }
 
-
-const addButton = document.querySelector('.add-button');
 addButton.onclick = addNewFieldset;
+
+
 
